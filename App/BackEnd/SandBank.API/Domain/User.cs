15068972ec783e97dfb2a0055f01484ec524bfb4
nicostouch <kanjiforbreakfast@live.com>
2019-08-21 21:22:00 +1200

    #1: add model and controller skeleton

---
+++ App/BackEnd/SandBank.API/Domain/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ App/BackEnd/SandBank.API/Endpoints/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Endpoints.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public ActionResult<User> Get(int id)
+        {
+            return default(User);
+        }
+        
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+        }
+    }
+}
