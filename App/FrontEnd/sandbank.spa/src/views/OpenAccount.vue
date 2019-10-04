938f03fd4644bcceb761aecbf499819900070968
nicostouch <kanjiforbreakfast@live.com>
2019-10-05 11:04:57 +1300

    41: open account works

--- App/FrontEnd/sandbank.spa/src/views/OpenAccount.vue
+++ App/FrontEnd/sandbank.spa/src/views/OpenAccount.vue
@@ -18,7 +18,10 @@
                 <el-input type="text" v-model="displayName" required></el-input>
             </el-form-item>
             <el-form-item>
-                <el-button type="primary" @click="openAccount()">Open account</el-button>
+                 <el-checkbox v-model="seedData">Seed dummy transaction data</el-checkbox>
+            </el-form-item>
+            <el-form-item>
+                <el-button v-loading="processing" type="primary" @click="openAccount()">Open account</el-button>
             </el-form-item>
         </el-form>
     </div>
@@ -27,12 +30,17 @@
 <script lang="ts">
 import Vue from 'vue';
 import Component from 'vue-class-component';
+import Axios, { AxiosResponse } from 'axios';
+import { Account } from '@/account';
+import { accountStore } from '@/store/store';
 
 @Component
 export default class Accounts extends Vue {
 
     private displayName: string = '';
     private accountType: string = 'TRANSACTION';
+    private seedData: boolean = true;
+    private processing: boolean = false;
 
     private get accountTypes() {
         return [
@@ -47,8 +55,61 @@ export default class Accounts extends Vue {
         ];
     }
 
-    private openAccount(): void {
-        // yolo
+    private async openAccount() {
+
+        this.processing = true;
+
+        const openAccountRequest = {
+            accountType: this.accountType,
+            displayName: this.displayName,
+        };
+
+        const headers = {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+        };
+
+        const account = await this.$http.post('/account', openAccountRequest, headers)
+        .then((response: AxiosResponse) => {
+            return response.data;
+        })
+        .catch((error) => {
+            alert(error);
+            return -1;
+        });
+
+        if (account.id > -1 && this.seedData) {
+            this.$http.post(`/account/${account.id}/seed`, {} , headers)
+            .then((response: AxiosResponse) => {
+                this.addAccountToStore(account);
+            })
+            .catch((error) => alert(error));
+        } else {
+            this.addAccountToStore(account);
+        }
+    }
+
+    private addAccountToStore(account: Account) {
+        Axios.all(
+                    [
+                        this.$http.get(`/account/${account.id}/balance`),
+                        this.$http.get(`/account/${account.id}/transaction`),
+                    ],
+                )
+                .then(Axios.spread((balanceResponse: AxiosResponse, transactionResponse: AxiosResponse) => {
+                    account.balance = balanceResponse.data;
+                    account.transactions = transactionResponse.data;
+
+                    this.$store.commit(`${accountStore}/addAccount`, account);
+
+                    this.$router.push('/accounts');
+                }))
+                .catch((error) => {
+                    account.balance = 0;
+                    account.transactions = [];
+                });
     }
 }
 </script>
