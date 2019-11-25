b35a5dba56c69fb236a1b86cc99a7a17cc35ac7d
James Tayler <james@tayler.io>
2019-11-26 00:47:56 +1300

    57: remove unecessary usings

--- App/BackEnd/SandBank.API/Endpoints/Program.cs
+++ App/BackEnd/SandBank.API/Endpoints/Program.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Database;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Services.Domain.Accounts;
 
 namespace Endpoints
