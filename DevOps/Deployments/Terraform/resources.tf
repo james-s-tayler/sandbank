a706432229cfaf23f0c71cb9dc6c08a8829e7619
James Tayler <james@tayler.io>
2019-11-30 00:32:08 +1300

    57: deploy vpc

--- DevOps/Deployments/Terraform/resources.tf
+++ DevOps/Deployments/Terraform/resources.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "subnet1" {
     cidr_block = var.subnet1_address_space
     map_public_ip_on_launch = "true"
     vpc_id = aws_vpc.vpc.id
-    availability_zone = data.availability_zones.available.names[0]
+    availability_zone = data.aws_availability_zones.available.names[0]
 }
 
 resource "aws_route_table" "rtb" {
@@ -26,4 +26,30 @@ resource "aws_route_table" "rtb" {
 resource "aws_route_table_association" "rtba" {
     subnet_id = aws_subnet.subnet1.id
     route_table_id = aws_route_table.rtb.id
+}
+
+resource "aws_security_group" "http-sg" {
+    name = "http-sg"
+    vpc_id = aws_vpc.vpc.id
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
